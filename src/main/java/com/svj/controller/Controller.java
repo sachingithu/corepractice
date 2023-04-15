715e4e4c80ae2f48f6c0da1b8e798166b87a7559
sachin <jadhavsachin5858@gmail.com>
2023-04-15 12:21:10 +0530

    controller class added...

---
+++ src/main/java/com/svj/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.svj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+	@GetMapping("/get")
+	public String getData(){
+		return "For Git Pract";
+	}
+}
