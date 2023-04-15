6db5c47e52e7b87770b177109c10dba67227dceb
sachin <jadhavsachin5858@gmail.com>
2023-04-15 11:53:45 +0530

    first commit

---
+++ src/main/java/com/svj/GitPractDemoApplication.java
@@ -0,0 +1,13 @@
+package com.svj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPractDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svj/GitPractDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPractDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
