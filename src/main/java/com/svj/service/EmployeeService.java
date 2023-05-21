f67fb9514c839a45370e475baf8c8ca4a49b2f0b
sachin <jadhavsachin5858@gmail.com>
2023-05-21 18:54:11 +0530

    Revert "Revert "method added to Service to save Employee...""
    This reverts commit 5cabacf7fc6d29122bb25502d6a6538c9a46cb90.

---
+++ src/main/java/com/svj/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.svj.service;
+
+import com.svj.model.Employee;
+
+public class EmployeeService implements IEmployeeService {
+
+	@Override
+	public void saveEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
